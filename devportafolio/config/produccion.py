f93160ee7e0fd8f881410ad69d7f4cc7b682cdd1
elvisdevB <elvis.burgos.dev@gmail.com>
2023-02-03 22:10:10 -0500

    Desarrollo Produccion

--- devportafolio/config/produccion.py
+++ devportafolio/config/produccion.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = 'django-insecure-(ab1@1cz7(p@41-6@!et)f)g6s1n5#rc4fo2tpf%6!)oh)ze&@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
